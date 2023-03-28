8bcaa3f3a5d2ff85c4c03291382c896cb58cced9
PsychicSalad <54758143+PsychicSalad@users.noreply.github.com>
2023-03-28 20:26:41 +0100

    started working on styling with styled components and css

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,43 +2,59 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 function Categories() {
-    return (
-        <List>
-            <NLink to={"/diet/gluten-free"}>
-                <h4>Gluten Free</h4>
-            </NLink>
-            <NLink to={"/diet/low-fodmap"}>
-                <h4>Low FODMAP</h4>
-            </NLink>
-            <NLink to={"/diet/vegan"}>
-                <h4>Vegan</h4>
-            </NLink>
-            <NLink to={"/diet/vegetarian"}>
-                <h4>Vegetarian</h4>
-            </NLink>
-        </List>
-    )
+  return (
+    <List>
+      <NLink to={"/diet/gluten-free"}>
+        <StyledHeader1>
+          <h4>Gluten Free</h4>
+        </StyledHeader1>
+      </NLink>
+      <NLink to={"/diet/low-fodmap"}>
+        <StyledHeader1>
+          <h4>Low FODMAP</h4>
+        </StyledHeader1>
+      </NLink>
+      <NLink to={"/diet/vegan"}>
+        <StyledHeader1>
+          <h4>Vegan</h4>
+        </StyledHeader1>
+      </NLink>
+      <NLink to={"/diet/vegetarian"}>
+        <StyledHeader1>
+          <h4>Vegetarian</h4>
+        </StyledHeader1>
+      </NLink>
+    </List>
+  );
 }
 
 const List = styled.div`
-    display: flex;
-    justify-content: center;
-    margin: 2rem 0rem;
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
 `;
 
 const NLink = styled(NavLink)`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-right: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-right: 2rem;
+  text-decoration: none;
+  border-bottom-style: none;
+  color: black;
 
-    &.active {
-
-        h4 {
-            color: green;
-        }
+  &.active {
+    h4 {
+      color: green;
     }
+  }
+`;
+
+const StyledHeader1 = styled.h1`
+  background-color: #68dede;
+  font-family: "Dela Gothic One", cursive;
+  font-size: 1.5rem;
 `;
 
 export default Categories;
